4665883a4188ac270c189a305090bb4f8d162995
Connor Kormos <connorkormos97@gmail.com>
2023-02-22 18:21:50 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cardsArr = [];
+
+
+const writeCards = (namesArr, event) => {
+    for (let i = 0; i < namesArr.length; i++){
+        cardsArr.push(`Thank you, ${namesArr[i]}, for the wonderful ${event} gift!`);
+    }
+    return cardsArr;
+}
+
+
+const countDown = (i) => {
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }  
+}
